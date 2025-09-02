e736ae9fb6f11946c1fa344e911d6d48799f24f3
Vinícius Cardoso <viniciuscarimateia@gmail.com>
2025-09-02 13:36:21 -0300

    Commit classes

---
+++ sistema-java/src/model/Conta.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Conta {
+    private int numero;
+    private double saldo;
+    private int clienteID;
+
+    public Conta(int numero, double saldo, int clienteID) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.clienteID = clienteID;
+    }
+
+}
